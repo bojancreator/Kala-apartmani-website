03961dd41e781b618871b2a54d9e71b4469decfe
bojancreator <bojan.creator@gmail.com>
2020-08-27 11:19:38 +0200

    promenjen krajnji datum u kalendaru

--- js/kala.js
+++ js/kala.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
   $('.datepicker').datepicker({
     format: 'dd/mm/yyyy', //Set Date Format
     startDate: new Date(), //Set Min Date Today
-    endDate: '18/12/2020', //Set Max Date
+    endDate: '18/12/2120', //Set Max Date
     datesDisabled: ['16/01/2017', '17/01/2017', '30/01/2017'], //Set Disabled Dates
     autoclose: true,
     todayHighlight: true,
